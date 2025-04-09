8ebb9ee35394eb4d0d4effbb16415c78c76c7c2a
ics-kitagawa <kitagawa@ics-web.jp>
2025-04-09 18:10:29 +0900

    cssファイルの分割、トーストの並び替え、リンクとヒントの追加

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // ツールチップの開閉を設定する
 const setupHints = () => {
-  const menuButtons = document.querySelectorAll(".menu-button");
+  const menuButtons = document.querySelectorAll(".js-menu-button");
+
   menuButtons.forEach((button) => {
+    // mouseenterでツールチップを表示する
     button.addEventListener("mouseenter", () => {
-      const tooltip = button.parentElement.querySelector(".menu-hint");
+      const tooltip = button.parentElement.querySelector(".js-menu-hint");
       if (tooltip && !tooltip.matches(":popover-open")) {
         tooltip.showPopover();
       }
     });
 
     button.addEventListener("mouseleave", () => {
-      const tooltip = button.parentElement.querySelector(".menu-hint");
+      // mouseleaveでツールチップを非表示にする
+      const tooltip = button.parentElement.querySelector(".js-menu-hint");
       if (tooltip && tooltip.matches(":popover-open")) {
         tooltip.hidePopover();
       }
@@ -20,12 +23,13 @@ const setupHints = () => {
 
 // トーストの設定
 const setupToast = () => {
-  const toastButton = document.querySelector(".toast-button");
+  const toastButton = document.querySelector(".js-toast-button");
 
+  // ボタン押下でトーストを表示する
   toastButton.addEventListener("click", () => {
     const toast = document.createElement("div");
     toast.popover = "manual";
-    toast.classList.add("toast");
+    toast.classList.add(["toast", "js-toast"]);
     const message = document.createElement("p");
     message.textContent = "トーストです！";
     toast.appendChild(message);
@@ -33,14 +37,49 @@ const setupToast = () => {
     document.body.appendChild(toast);
     toast.showPopover();
 
+    // 2秒後に自動的に非表示にする
     window.setTimeout(() => {
       toast.hidePopover();
       toast.remove();
-    }, 2000)
-  })
-}
+    }, 2000);
+
+    // 表示/非表示でトーストを並べる
+    toast.addEventListener("toggle", (event) => {
+      alignToast(event.newState === "closed");
+    });
+  });
+
+  // トーストを順番に縦に並べる
+  const alignToast = (withMoveAnim) => {
+    const toasts = document.querySelectorAll(".js-toast");
+    toasts.forEach((toast, index) => {
+      toast.style.transition = withMoveAnim
+        ? "translate 0.2s linear, opacity 0.2s linear"
+        : "opacity 0.2s linear";
+      toast.style.translate = `-50% ${(60 + 10) * index}px`;
+      toast.style.opacity = 1;
+    });
+  };
+};
+
+// リンクのヒントを表示する設定
+const setupLinkHint = () => {
+  const help = document.querySelector(".js-link-help");
+  const hint = document.querySelector(".js-link-hint");
+  help.addEventListener("mouseover", () => {
+    if (hint && !hint.matches(":popover-open")) {
+      hint.showPopover();
+    }
+  });
+  help.addEventListener("mouseleave", () => {
+    if (hint && hint.matches(":popover-open")) {
+      hint.hidePopover();
+    }
+  });
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   setupHints();
   setupToast();
+  setupLinkHint();
 });
